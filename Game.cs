2f5ace2fed6cdddbd9c7d1964394c43bfc233a99
Cas0n-McVey <absolgamer25@gmail.com>
2025-04-04 13:58:46 -0500

    Constructors

---
+++ Game.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Constructors_McVey_Cason
+{
+    class Game
+    {
+        // Basic games have a title and a genre
+        public string title, genre;
+
+        // If any code don't have a title and genre will have that string
+        public Game(string titleParam = "Unknown Title", string genreParam = "Casual")
+        {
+            // To make it shorter and easier instead of titleParam  and genre Param
+            title = titleParam;
+            genre = genreParam;
+
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Constructors_McVey_Cason
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Have my title and genre because it have both title and genre in Game.cs
+            Game myGame = new Game("Terraria", "T for Teen");
+
+            // To paint my string and myGame code
+            Console.WriteLine($"My favorite game is {myGame.title} and it is rated {myGame.genre}!");
+
+            // To show you what happens if you don't have a title and genre and call it a different variable
+            Game myOtherGame = new Game();
+
+            // To paint what happen if we don't have a title and genre
+            Console.WriteLine($"My other favorite game is {myOtherGame.title} and it is rated {myOtherGame.genre}!");
+        }
+    }
+}
